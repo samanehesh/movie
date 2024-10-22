a8b22be890da5f78527c892752afd3524a3d3841
samanehesh <145700227+samanehesh@users.noreply.github.com>
2024-10-22 11:53:28 -0700

    api key

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -22,6 +22,8 @@ export const fetchSearchedMovies = async (searchQuery) => {
 // Fetch popular movies
 export const fetchPopularMovies = async () => {
   try {
+    console.log("API Key in production:", import.meta.env.VITE_TMDB_API_KEY);
+
     const res = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
     return res.data.results;
   } catch (error) {
